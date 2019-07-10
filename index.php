e83f8a926bf0794a1b69dbddb06de8eea4ccfd49
hieuluu-lulu <52305737+hieuluu-lulu@users.noreply.github.com>
2019-07-10 16:54:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>HiHi</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style type="text/css" media="screen">
+    	@charset "utf-8";
+* {
+	margin: 0;
+	padding: 0;
+	pointer-events: none;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-o-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+}
+body{
+	position: relative;
+    background-color: #000;
+    overflow: hidden;
+}
+dl {
+	width: 230px;
+	height: 340px;
+	position: relative;
+	margin: 150px auto 0;
+	-webkit-transform-style: -webkit-preserve-3d;
+	transform-style: preserve-3d;
+}
+dd {
+	width: 230px;
+	height: 340px;
+	position: absolute;
+	top: 0;
+	left: 0;
+}
+img{width:100%;height:100%;}
+dt {
+	width: 900px;
+	height: 900px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-50%) rotateX(90deg) translateZ(-200px);
+	background: -moz-radial-gradient(center center,farthest-side,#fff5,#0005);
+	background: -webkit-radial-gradient(center center,farthest-side,#fff5,#0005);
+	background: -o-radial-gradient(center center,farthest-side,#fff5,#0005);
+	background: radial-gradient(center center,farthest-side,#fff5,#0005);
+}
+.inverteds {
+	transform: scaleY(-1);
+	position: absolute;
+	left: 0;
+	top: 370px;
+	width: 230px;
+	height: 340px;
+	opacity:0.5;
+}
+.inverted {
+	position: relative;
+	width: 230px;
+	height: 340px;
+}
+.inverted::before {
+	content: '';
+	display: block;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	background: -moz-radial-gradient(bottom,circle farthest-side,#0000,#000 66%);
+	background: -webkit-radial-gradient(bottom,circle farthest-side,#0000,#000 66%);
+	background: -o-radial-gradient(bottom,circle farthest-side,#0000,#000 66%);
+	background: radial-gradient(bottom,circle farthest-side,#0000,#000 66%);
+}
+.name{
+	color: #fff;
+	text-align: center;
+	margin-top: 400px;
+}
+.content-carrousel{
+    width: 100%;
+    position: absolute;
+    float: right;
+    animation: rotar 15s infinite linear;
+    transform-style: preserve-3d;
+}
+.content-carrousel:hover{
+    animation-play-state: paused;
+    cursor: pointer;
+}
+@keyframes rotar{
+    from{
+        transform: rotateY(0deg);
+    } to{
+        transform: rotateY(360deg);
+    }
+}
+@media(min-width: 300px){
+	dd {
+		width: 200px;
+		height: 310px;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+}
+@media(min-width: 1200px){
+	dd {
+		width: 230px;
+		height: 340px;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+}
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="jquery.min.js" type="text/javascript"></script>
+
+<body>
+    <dl>
+        <dt></dt>
+        <div class="content-carrousel">
+            <dd><img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/56331102_1198326843653364_714626203788509184_n.jpg?_nc_cat=111&_nc_eui2=AeGZvH58PwQu1kiaPcxLU7f6iN49-mqYTiZkqRtsweQeM6p1iL2fPxEwScoz7QfT6QBVrgyNETWujgxwkBika0RoPcnc0I9LuExGVSuv6T7Ffg&_nc_oc=AQn3zfvdnNUkEIdbUiwnP7g__VTZVlg5-QfgquiaWn9aSp-IOD-2EsCJU_uIG9y6Qb5KdVXZR96qoDT_civRMcXW&_nc_ht=scontent.fdad2-1.fna&oh=a389a272160b5f91430b1ac7d532d391&oe=5DA69144" /></dd>
+            <dd><img src="https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/62520613_1249601795192535_4792213655770890240_n.jpg?_nc_cat=105&_nc_eui2=AeH6Tcg1DuhlzGlBT71mHR9wePREceHXTkT-ZLNGhr2zhpMKHwNeoRwBT-CE1uvcudogIj7uIfuYX5EG4b1d8txyhdjL9cqRJ5kUumHXVQRDwQ&_nc_oc=AQnRRwnPVbmfg0rE4357JXk1pSy_nDexP80qPJgVICArR_gbrv2jO3AzaJWry9MNoyI3C2orFrN_K1Ze-H6U_XQl&_nc_ht=scontent.fdad1-1.fna&oh=c563d528f3f84542ccc791b3152fa765&oe=5DB9585C" /></dd>
+            <dd><img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/64483895_1249601811859200_239193815410278400_n.jpg?_nc_cat=107&_nc_eui2=AeHh2Dq6muJs9kIVP9hnNraWdXazjYcbszGQhvYuFqngHh1WLXNudmEAta-X5s5s1PVf0Z0LoPRjVxNLaZINdZBvNIbZ7EPQC0JdYPClhUNXAQ&_nc_oc=AQnEJ4WFOBfFewTMqvwKiU7hV-htx70f7tWtl4eNSpQ9ofbYxcJtLik-jYJlDCda1poZoHf17GOCpIc_IM3xJkB5&_nc_ht=scontent.fdad2-1.fna&oh=b80136d48c90afad6f20a2b74c53bb2a&oe=5DAD2725" /></dd>
+            <dd><img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/55935578_1198326833653365_1965704332632391680_n.jpg?_nc_cat=110&_nc_eui2=AeF3ettn_TiMzijETAHWA0i-inIiPIjWv3Twq-AZ3vkvu7R3OfYpUnACl3a6tWSBlV9q7cmsrVw2-LywYEge2jNmmhrA9hHbmFfgUbODNCEAyA&_nc_oc=AQmYeVGDc66B5WeZMO8R9u2qviVGgYnebSffBfohp4rUe7GByfEu6dseZehjstTh12o9DgkdQ8jEJB8E9awkzupK&_nc_ht=scontent.fdad2-1.fna&oh=a5d385f173ae801e7caa03bc909e00d9&oe=5DC4D5B9" /></dd>
+            <dd><img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/62321050_1248089985343716_449282656090193920_n.jpg?_nc_cat=108&_nc_eui2=AeEztoNL4L5OHeQ__tPBMo0vzbtqvlmIspL21odLho-huj8rrpxtJ17iNGAnthvFyaRozq6d8izrCFMLiMFsI-N9H9p4yFIRQMxBjb14y495DQ&_nc_oc=AQmLFcNpcuw81YWc1JL8yGyP7bN-tN2Btwd1zGFSSJ40DulVNSIpd5VtHQnEwEgxL3bXLJ0C96h9JsQJfsCBJvyR&_nc_ht=scontent.fdad2-1.fna&oh=38a41e1e63d1d099480d42f45cb8df69&oe=5DB903C1" /></dd>
+            <dd><img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.0-9/59938217_1225467567605958_5335979214346649600_n.jpg?_nc_cat=110&_nc_eui2=AeE4GXXBY1X09ifJHvBDdIOdIu187ConnZ45ySENHrmBSt_Etc1zvURkNTqYsu9-qn2dQdfxje2Lar9dEXtEjj1dq4qD6qw7_I__2Nseiqhnpw&_nc_oc=AQlZdYBlg2-2YQid__nTuz9ByG_XW47OigIZtqICe7hr7VUDS3YoQ2D6P4g0ssRwlkrPRP6B623vYFWxDObI1C7_&_nc_ht=scontent.fdad2-1.fna&oh=6413f610dffdf0446cf5f3ea2688c754&oe=5DAF1FA0" /></dd>
+            <dd><img src="https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/62258321_1248089382010443_7747756071497760768_n.jpg?_nc_cat=105&_nc_eui2=AeHuSj9M5U1ouq3nvDwhGnVwIgT1OAdPxdCvNTm8uivAXn9SDLHoAxq61Sn805DTZM8z592TYmsecOldojVvGTtwkTBLkXZZHGNBMlDFovVbcA&_nc_oc=AQmZmlRgpNEIXeABLrAgo_k4BS8wfZxSW53Jd7B3l9wY4Ut7lHmRKJhovpj618782RMjtWBDehO7_iFW0hbzIOyE&_nc_ht=scontent.fdad1-1.fna&oh=0ab1af7af6ba64949f7d0859b3dd34e7&oe=5D7D779C" /></dd>
+            <dd><img src="https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/62102060_1248089148677133_7372059025665425408_n.jpg?_nc_cat=102&_nc_eui2=AeGWdZx1nC6lGx0tVtI62gXnIuD9VUUrWoldLluhtJ_ab0564-0Cd1HK95mfi3QP98ZZVjJjyoA9sfUQ45xsKc0jMdrqPkQZvTAR1O5glRiNcA&_nc_oc=AQmmaJKCWt1s9dYaJKpByE1zvf8xALsjrHXgA-H2meqPJOitL_sbCuZEnJBOSfzeBFaHqv7dROzGa9x_YOYXZpxj&_nc_ht=scontent.fdad1-1.fna&oh=f21ee74d0b626c0e27a83a67596b7a42&oe=5DB4C2E7" /></dd>
+            <dd><img src="https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/62517525_1248090132010368_2458827467964547072_n.jpg?_nc_cat=100&_nc_eui2=AeE86XMOSM0cKlviZ5mTvKBWdVXyNgXmY2N2AFSkzGJAF42Q6LpJADwS19NeqGk1edoIEt3CjxQ_CboyToEEp0e6zTu2rkscIY6xQjX1atDPbQ&_nc_oc=AQm98-qfpzomRSY2k-gO7lp6oJSRYJxVK-B7o81Kb0hGEn2KRRy4ocIUNAApWjUIfFfaY_TdOPwCspIJi_iTIbbv&_nc_ht=scontent.fdad1-1.fna&oh=cee5c88298bf3cf6e009fca7bb2f3310&oe=5DBB2673" /></dd>
+            <dd><img src="https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/62511503_1248089842010397_7471570896767942656_n.jpg?_nc_cat=103&_nc_eui2=AeFhpADVeabP7z5PrGJkjYpr6po4O1GB1d07rmHlbiKe4lLFTYby9Anuxz0GCXhSrrNn6uSJ3s6us4ZgF6BWpNnbW2zuQ0ic6pW32GA9hgcXiQ&_nc_oc=AQl4NBeipfzefJq5_JeKYkQqpAU2IxEAW6DYamXIyYGxMxwUxe9bbUBDXUvGVX8CC8gymn5d2QrlfKkmi6IBQE75&_nc_ht=scontent.fdad1-1.fna&oh=0a79567865fc043a526a883cc854af7f&oe=5DA9BDAA" /></dd>
+        </div>
+    </dl>
+    <div class="name">Thi Thi</div>
+    <embed src="silence.mp3" type="audio/mpeg" autostart="true" hidden="true">
+        <audio id="player" autoplay loop>
+            <source src="1.mp3" type="audio/mpeg">
+        </audio>
+
+    
+  
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</head>
+</html>
